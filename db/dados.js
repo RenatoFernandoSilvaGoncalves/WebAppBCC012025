c82754c9df34eda2b2331a3a19e94ce542f7d9e8
renatofernandosilvagoncalves <renatogoncalves@unoeste.edu.br>
2025-05-27 09:33:11 -0300

    implementado a exclusão do cliente

--- db/dados.js
+++ db/dados.js
@@ -53,15 +53,6 @@
       "cidade": "Presidente Prudente",
       "uf": "SP",
       "cep": "11666-666"
-    },
-    {
-      "id": "3401",
-      "cpf": "777.777.777-77",
-      "nome": "Teste de cadastro",
-      "telefone": "179777-7777",
-      "cidade": "São José do Rio Preto",
-      "uf": "SP",
-      "cep": "17777-777"
     }
   ],
   "fornecedores": [],
